3b8db42375c78807750d2f537ddb85eafa793d39
Brandi Phillips <milligan.brandi@gmail.com>
2016-08-25 10:51:59 -0700

    trying again

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,29 @@
+# creating a Queue
+
 class Queue
   def initialize
+    @store = Array.new
   end
-  
+
   def enqueue(element)
+    @store.push(element)
+    # option2: @store.unshift(element)
   end
- 
+
   def dequeue
+    return @store.shift
+    # option2: return @store.pop
   end
 
   def front
+    return @store(0)
   end
- 
+
   def size
+    return @store.length
   end
 
   def empty?
+    return size == 0
   end
 end
